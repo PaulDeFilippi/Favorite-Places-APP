5d26db4da46052682ab813e83808f1db6fc78fa1
Paul DeFilippi <PDImac@zlss-mbp.home>
2016-08-23 15:41:56 -0400

    Added files for Favorite Places APP

---
+++ favorite-place/DetailVC.swift
@@ -0,0 +1,40 @@
+//
+//  DetailVC.swift
+//  favorite-place
+//
+//  Created by Paul DeFilippi on 8/21/16.
+//  Copyright © 2016 Paul DeFilippi. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
